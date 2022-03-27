2170be1efcba2d2bfeef205d08ccd4824d88f4db
June <june.qualwebs@gmail.com>
2022-03-27 12:29:53 +0530

    Adding header and font awesome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-
+import {Container} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      My APP
+      <main className="py-3">
+        <Container>
+          <h1>My APP</h1>
+        </Container>
+      </main>
+      
       <Footer/> 
     </div>
   );
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import { Container, Navbar, Nav } from "react-bootstrap";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+        <Container>
+          <Navbar.Brand href='/'>ProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link href='/cart'>
+                <i className='fas fa-shopping-cart'></i>
+                Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i className='fas fa-user'></i>
+                Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
