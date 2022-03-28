d570ebfe2936214e76e26c683784a3ad86ec9bbe
June <june.qualwebs@gmail.com>
2022-03-28 23:02:52 +0530

    data seeder and mongoose setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import products from  './data/products.js';
+import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 const app = express();
 
-dotenv.config()
+dotenv.config();
+
+connectDB();
+
 app.get('/',(req,res)=>{res.send('API is runnig');} )
 
 app.get('/api/products', (req,res)=>{
